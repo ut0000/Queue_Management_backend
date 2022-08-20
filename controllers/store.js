dc151873dbc312c4fc1e57abd4b825ca304df8c4
golchi <golchigeek@gmail.com>
2022-08-20 17:50:48 +0530

    Add user in queue

--- controllers/store.js
+++ controllers/store.js
@@ -48,3 +48,55 @@ exports.makestore = async (req,res,next)=>{
         next(err);
     }
 }
+
+
+exports.adduser = async ( req,res,next)=>{
+    try{
+        const shopid = req.body.shopid;
+        const userid = req.body.userid;
+        const time = req.body.time;
+        result = await shop.findOne({_id:shopid});
+        if(!result){
+            res.json('no shop exist');
+            console.log('no shop exist');
+        }
+        else{
+            var mini = result.ShopCounter[0]*result.avgtime[0];
+            var counter = 0 , i;
+            let tt = 1;
+            for(var j = 0 ; j< result.counter ; j++ ){
+                if(result.ShopCounter[j]*result.avgtime[j]==0){
+                    result.avgtime[j]++;
+                    counter = j;
+                    console.log(j);
+                    tt = 0;
+                }
+            }
+            await result.save();
+            if(tt){
+                for(i = 0; i < result.counter ; i++){
+                    if(result.ShopCounter[i]*result.avgtime[i]<mini){
+                        mini = result.ShopCounter[i]*result.avgtime[i];
+                        counter = i;
+                    }
+                }
+            }
+            result.ShopCounter[counter]++;
+            var pos =  result.ShopCounter[counter]
+            ans = await users.findById(userid);
+            if(!ans){
+                console.log('no user exist')
+                return res.json("no user exist");
+            }
+            result.queue.push({_id:userid,counter,time,pos});
+            await result.save();
+            res.json({counter:counter});
+        }
+    }
+    catch(err){
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+}
